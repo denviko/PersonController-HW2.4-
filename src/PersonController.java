be9ba49345177fb9b6a8b6d2a79f437f4d653b03
DenViko <DenViko@yahoo.com>
2023-04-02 09:42:47 +0500

    HW2.4

---
+++ src/PersonController.java
@@ -0,0 +1,11 @@
+public class PersonController {
+    Person person = new Person("DenViko", "P123456", "P123456");
+
+
+    public static void main(String[] args) {
+    }
+
+    public Person enterID () {
+        return null;
+    }
+}
